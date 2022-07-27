2ef7501122065dc48df52f881f6bc550452aff8f
AidanCrozier <aidand.crozier@gmail.com>
2022-07-27 17:58:01 -0400

    Revert "project commit"
    This reverts commit 41c3bf6080ed4a5d17a79a81146c5773b8cdbe0d.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.acrozierceilingfan {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.acrozierceilingfan to javafx.fxml;
-    exports com.example.acrozierceilingfan;
-}
